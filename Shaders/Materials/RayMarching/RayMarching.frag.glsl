e6fff4fb1b3dd703f52b166dd425621081dc2e01
Nicolas Mellado <nmellado0@gmail.com>
2019-02-27 15:37:51 +0100

    Improve RayMarching shader
     - add color map

--- Shaders/Materials/RayMarching/RayMarching.frag.glsl
+++ Shaders/Materials/RayMarching/RayMarching.frag.glsl
@@ -19,6 +19,37 @@ layout (location = 3) in vec3 in_cameraInModelSpace;
 
 float stepsize = 0.01;
 
+// source: https://github.com/kbinani/colormap-shaders/blob/master/shaders/glsl/MATLAB_jet.frag
+float colormap_red(float x) {
+    if (x < 0.7) {
+        return 4.0 * x - 1.5;
+    } else {
+        return -4.0 * x + 4.5;
+    }
+}
+
+float colormap_green(float x) {
+    if (x < 0.5) {
+        return 4.0 * x - 0.5;
+    } else {
+        return -4.0 * x + 3.5;
+    }
+}
+
+float colormap_blue(float x) {
+    if (x < 0.3) {
+       return 4.0 * x + 0.5;
+    } else {
+       return -4.0 * x + 2.5;
+    }
+}
+
+vec4 colormap(float x) {
+    float r = clamp(colormap_red(x), 0.0, 1.0);
+    float g = clamp(colormap_green(x), 0.0, 1.0);
+    float b = clamp(colormap_blue(x), 0.0, 1.0);
+    return vec4(r, g, b, 1.0);
+}
 
 vec4 shade(vec3 position, in float value) {
 //    if ( value > 1)
@@ -36,16 +67,18 @@ void main(void) {
 	vec3 raypos = in_texcoord;//in_position;
     vec3 camera = in_cameraInModelSpace;
     vec3 raydir =in_position - camera;
-    raydir.x /= 4;
+    raydir.x /= 4.5;
     raydir = normalize(raydir) * stepsize;
 
+    float accum = 0.;
     for(int i = 0;
         i<1000;
         i++) {
         value = texture(material.buffer, raypos).r;
         if ( value != 0. )
         {
-            frgColor += vec4(value);
+            accum += value;
+//            frgColor += shade(raypos,value);
             hit = true;
         }
 
@@ -59,7 +92,8 @@ void main(void) {
 
     if ( ! hit ) discard;
 
-    fragColor = frgColor;
+//    fragColor = frgColor;
+    fragColor = colormap ( clamp(accum/5., 0., 1.) );
 
     out_normal = vec4(normalize(raydir), 1.0);
     out_diffuse = vec4(normalize(in_position), 1.0);
